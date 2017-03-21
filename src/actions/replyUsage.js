66479c0ff1e83cc8afb363f9219baa71b7543955
Wei Zhu <yesmeck@gmail.com>
2017-03-22 00:00:11 +0800

    Update comment

--- src/actions/replyUsage.js
+++ src/actions/replyUsage.js
@@ -4,9 +4,9 @@ const { commentIssue, closeIssue } = require('../github');
 const comment = "\
 Hello @{user}. We use GitHub issues to trace bugs or discuss \
 plans of Ant Design. So, please don't ask usage questions here. You can try \
-to ask questions in [Stack Overflow](http://stackoverflow.com/questions/tagged/antd) \
+to ask questions on [Stack Overflow](http://stackoverflow.com/questions/tagged/antd) \
 or [Segment Fault](https://segmentfault.com/t/antd), then apply tag `antd` and \
-`react` to your questions.";
+`react` to your question.";
 
 function replyUsage(on) {
   on('issues_labeled', (payload) => {
