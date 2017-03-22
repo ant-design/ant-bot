a1dc2999947c78bb498fa0e21f2b8bd9a4fffb84
Wei Zhu <yesmeck@gmail.com>
2017-03-22 20:46:37 +0800

    Typo

--- src/actions/replyNeedDemo.js
+++ src/actions/replyNeedDemo.js
@@ -2,8 +2,8 @@ const format = require('string-template');
 const { commentIssue } = require('../github');
 
 const comment = {
-  'ant-design': 'Hellp @{user}. Please provide a re-producible demo: http://codepen.io/benjycui/pen/KgPZrE?editors=001',
-  'ant-design-mobile': 'Hellp @{user}. Please provide a re-producible demo: http://codepen.io/paranoidjk/pen/LWpaKe',
+  'ant-design': 'Hello @{user}. Please provide a re-producible demo: http://codepen.io/benjycui/pen/KgPZrE?editors=001',
+  'ant-design-mobile': 'Hello @{user}. Please provide a re-producible demo: http://codepen.io/paranoidjk/pen/LWpaKe',
 }
 
 function replyNeedDemo(on) {
