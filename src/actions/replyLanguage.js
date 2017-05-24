36d4bd3a6512b204b63879aa29b5587c65d37eb6
Benjy Cui <benjytrys@gmail.com>
2017-05-24 16:20:43 +0800

    feat: reply when issue is Chinese, close: #1

---
+++ src/actions/replyLanguage.js
@@ -0,0 +1,23 @@
+const { commentIssue } = require('../github');
+
+const comment = `
+It will be better to write your issue/comment in English, so more people can understand you.
+ And this means that more people can help you or benefit from your issue/comment.
+ See: https://github.com/ant-design/ant-design/issues/4897`
+
+function containsChinese(title) {
+  return /[\u4e00-\u9fa5]/.test(title);
+}
+
+function replyLanguage(on) {
+  on('issues_opened', ({ payload }) => {
+    if (containsChinese(payload.issue.title)) {
+      commentIssue(
+        payload,
+        comment
+      );
+    }
+  });
+}
+
+module.exports = replyLanguage;
