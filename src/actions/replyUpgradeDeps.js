d33e4b36a6874236d24a099bd651d9f2dc979bbb
Wei Zhu <yesmeck@gmail.com>
2017-06-01 16:17:28 +0800

    escape

--- src/actions/replyUpgradeDeps.js
+++ src/actions/replyUpgradeDeps.js
@@ -4,8 +4,8 @@ const { commentIssue } = require('../github');
 const reply = `
 Hello @{user}.
 
-- If you use npm, please try `rm -rf node_modules && npm install` to upgrade all your deps.
-- If you use yarn, you may need `yarn upgrade`.
+- If you use npm, please try \`rm -rf node_modules && npm install\` to upgrade all your deps.
+- If you use yarn, you may need \`yarn upgrade\`.
 
 <img src="https://cloud.githubusercontent.com/assets/465125/26667345/4bcc8f10-46d7-11e7-8c72-32a0c68ea7ca.jpg" width="500" height="300">
 `;
