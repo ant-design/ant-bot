68efa661011c4bb9fb0acd8479dd0550a36a0751
Wei Zhu <yesmeck@gmail.com>
2017-10-26 15:32:42 +0800

    Add logger

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const Koa = require('koa');
 const Router = require('koa-router');
 const bodyParser = require('koa-bodyparser');
+const logger = require('koa-logger');
 const requireDir = require('require-dir');
 const githubEvent = require('./githubEvent');
 const actions = requireDir('./actions');
@@ -17,6 +18,7 @@ router
   .post('/webhook/:repo', webhook);
 
 app
+  .use(logger())
   .use(bodyParser())
   .use(router.routes())
   .use(router.allowedMethods());
