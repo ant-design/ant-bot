e489e34dc10b9507066f6c8f8a2500f76351ee9e
Wei Zhu <yesmeck@gmail.com>
2017-12-06 10:35:15 +0800

    Use SENTRY_DSN

--- app/setup.js
+++ app/setup.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const Raven = require('raven');
 
-if (process.env.NODE_ENV === 'production') {
-  Raven.config(`https://${process.env.SENTRY_KEY}@sentry.io/254752`).install();
+if (process.env.SENTRY_DSN) {
+  Raven.config(process.env.SENTRY_DSN).install();
 }
