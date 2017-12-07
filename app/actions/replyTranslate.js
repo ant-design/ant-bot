b6db5e818042387c734d9c6ee2dabec8e88b0695
Wei Zhu <yesmeck@gmail.com>
2017-12-07 17:49:39 +0800

    Add translate

---
+++ app/actions/replyTranslate.js
@@ -0,0 +1,21 @@
+const translate = require('google-translate-api');
+const { mentioned } = require('../../lib/utils');
+const { commentIssue } = require('../../lib/github');
+
+function replyTranslate(on) {
+  on('issue_comment_created', async ({ payload }) => {
+    if (mentioned(payload.comment.body) && payload.comment.body.includes('translate')) {
+      const content = `## ${payload.issue.title}\n${payload.issue.body}`;
+      const res = await translate(content, { from: 'zh-CN', to: 'en' });
+      console.log(res);
+      commentIssue({
+        owner: payload.repository.owner.login,
+        repo: payload.repository.name,
+        number: payload.issue.number,
+        body: res.text,
+      });
+    }
+  });
+}
+
+module.exports = replyTranslate;
