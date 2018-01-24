55dfff0d8fba50533fde187dbda1a06a8caa4b3f
Wei Zhu <yesmeck@gmail.com>
2018-01-24 16:35:55 +0800

    Emit all events

--- app/routes/webhook.js
+++ app/routes/webhook.js
@@ -8,12 +8,10 @@ module.exports = function webhook(ctx) {
     const action = payload.action;
     eventName += `_${action}`;
     console.log('receive event: ', eventName);
-    if (payload.sender.login !== process.env.GITHUB_BOT_NAME) {
-      githubEvent.emit(eventName, {
-        repo: ctx.params.repo,
-        payload,
-      });
-    }
+    githubEvent.emit(eventName, {
+      repo: ctx.params.repo,
+      payload,
+    });
     ctx.body = 'Ok.';
   } else {
     ctx.body = 'Go away.';
