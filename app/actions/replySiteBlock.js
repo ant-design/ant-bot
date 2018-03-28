9d5ad390745bc40c0d387fffbb70520742efeaa2
偏右 <afc163@gmail.com>
2018-03-28 13:08:29 +0800

    Update replySiteBlock.js

--- app/actions/replySiteBlock.js
+++ app/actions/replySiteBlock.js
@@ -12,6 +12,8 @@ function replySiteBlock(on) {
   on('issues_opened', async ({ payload }) => {
     if (containsSiteBlock(payload.issue.title)) {
       const content = `
+Ant Design 系列官网由于某些众所周知的原因无法访问，建议翻墙或访问国内镜像站点。
+
 ## 官网
 
 * Ant Design: https://ant.design
