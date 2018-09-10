5aceaa7087924124c27150cf847c20a6ebb545d0
偏右 <afc163@gmail.com>
2018-09-10 19:28:13 +0800

    Update replyInvalid.js

--- app/actions/replyInvalid.js
+++ app/actions/replyInvalid.js
@@ -8,7 +8,7 @@ Hello @{user}, your issue has been closed because it does not conform to our \
 issue requirements. Please use the [Issue Helper](http://new-issue.ant.design?repo={repo}) \
 to create an issue, thank you! \n\n\
 你好 @{user}，为了能够进行高效沟通，我们对 issue 有一定的格式要求，你的 issue 因为不符合要求而被自动关闭。\
-你可以通过[issue 助手](http://new-issue.ant.design?repo={repo}) 来创建 issue 以方便我们定位错误。谢谢配合！';
+你可以通过 [issue 助手](http://new-issue.ant.design?repo={repo}) 来创建 issue 以方便我们定位错误。谢谢配合！';
 
 let members = [];
 const repos = ['ant-design'];
