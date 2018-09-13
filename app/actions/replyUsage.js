6f7ed65d4e0d4e15e2a9aef91ae2d791be1d55f2
zombiej <smith3816@gmail.com>
2018-09-13 22:17:44 +0800

    add usage chinese

--- app/actions/replyUsage.js
+++ app/actions/replyUsage.js
@@ -7,7 +7,14 @@ Hello @{user}, we use GitHub issues to trace bugs or discuss \
 plans of Ant Design. So, please [don't ask usage questions](https://github.com/ant-design/ant-design/issues/2320) here. You can try \
 to ask questions on [Stack Overflow](http://stackoverflow.com/questions/tagged/antd) \
 or [Segment Fault](https://segmentfault.com/t/antd), then apply tag `antd` and \
-`react` to your question.";
+`react` to your question.\
+\n\
+\n\
+你好 @{user}，Ant Design Issue 板块是用于 bug 反馈与需求讨论的地方。\
+请[勿询问如何使用的问题](https://github.com/ant-design/ant-design/issues/2320)，\
+你可以试着在 [Stack Overflow](http://stackoverflow.com/questions/tagged/antd) 或者 [Segment Fault](https://segmentfault.com/t/antd) \
+中提问（记得添加 `antd` 和 `react` 标签哦~）。\
+";
 
 function replyUsage(on) {
   on('issues.labeled', ({ payload }) => {
