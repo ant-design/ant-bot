df7b2035b11e2d1211ff0f0e248317c1332496be
zombiej <smith3816@gmail.com>
2018-09-13 21:57:59 +0800

    add chinese

--- app/actions/replyNeedReproduce.js
+++ app/actions/replyNeedReproduce.js
@@ -4,6 +4,8 @@ const { commentIssue } = require('../../lib/github');
 const comment = {
   'ant-design': `
 Hello @{user}. Please provide a online reproduction by forking this link https://u.ant.design/codesandbox-repro. Issues labeled by \`Need Reproduce\` will be closed if no activities in 7 days.
+
+你好 @{user}, 我们需要你提供一个在线的重现实例以便于我们帮你排查问题。你可以通过点击[此处](https://u.ant.design/codesandbox-repro)创建。7 天内未跟进的 issue 将会被自动关闭。
 `,
   'ant-design-mobile':
     'Hello @{user}. Please provide a re-producible demo: https://codepen.io/pen?template=LWpaKe&editors=0010',
