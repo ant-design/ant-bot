857913602cf57dd13c073c336e7d01e596b04b6d
Wei Zhu <yesmeck@gmail.com>
2018-12-26 00:13:44 +0800

    有 changelog 才发钉钉

--- app/actions/makeRelease.js
+++ app/actions/makeRelease.js
@@ -24,17 +24,19 @@ function makeRelease(on) {
       name: version,
       body: changelog,
     });
-    // Notify dingding
-    axios.post(
-      `https://oapi.dingtalk.com/robot/send?access_token=${process.env.DINGDING_BOT_TOKEN}`,
-      {
-        msgtype: 'markdown',
-        markdown: {
-          title: `${version} 发布日志`,
-          text: `# ${version} 发布日志 \n\n ${cnChangelog}`,
+    if (cnChangelog) {
+      // Notify dingding
+      axios.post(
+        `https://oapi.dingtalk.com/robot/send?access_token=${process.env.DINGDING_BOT_TOKEN}`,
+        {
+          msgtype: 'markdown',
+          markdown: {
+            title: `${version} 发布日志`,
+            text: `# ${version} 发布日志 \n\n ${cnChangelog}`,
+          },
         },
-      },
-    );
+      );
+    }
   });
 }
 
