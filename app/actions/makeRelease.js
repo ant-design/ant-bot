49556b8b4af81d630bcf1fcd1f0cb5f2c7368010
二货机器人 <smith3816@gmail.com>
2020-01-06 14:42:18 +0800

    feat: Notify both v3 & v4 (#63)
    * feat: Notify both v3 & v4
    
    * use branchs

--- app/actions/makeRelease.js
+++ app/actions/makeRelease.js
@@ -1,6 +1,9 @@
+/* eslint-disable no-await-in-loop */
 const axios = require('axios');
 const { getChangelog } = require('../../lib/utils');
 const { createRelease } = require('../../lib/github');
+
+const branches = ['master', '3.x-stable'];
 
 function makeRelease(on) {
   on('create', async ({ payload, repo }) => {
@@ -11,11 +14,19 @@ function makeRelease(on) {
       return;
     }
     const version = payload.ref;
-    const url = 'https://raw.githubusercontent.com/ant-design/ant-design/master/';
-    const enChangelogContent = await axios.get(`${url}/CHANGELOG.en-US.md`);
-    const enChangelog = getChangelog(enChangelogContent.data, version);
-    const cnChangelogContent = await axios.get(`${url}/CHANGELOG.zh-CN.md`);
-    const cnChangelog = getChangelog(cnChangelogContent.data, version);
+
+    let enChangelog = '';
+    let cnChangelog = '';
+
+    for (let i = 0; i < branches.length; i += 1) {
+      const url = `https://raw.githubusercontent.com/ant-design/ant-design/${branches[i]}/`;
+
+      const enChangelogContent = await axios.get(`${url}/CHANGELOG.en-US.md`);
+      enChangelog = enChangelog || getChangelog(enChangelogContent.data, version);
+      const cnChangelogContent = await axios.get(`${url}/CHANGELOG.zh-CN.md`);
+      cnChangelog = cnChangelog || getChangelog(cnChangelogContent.data, version);
+    }
+
     const changelog = [enChangelog, '\n', '---', '\n', cnChangelog].join('\n');
     createRelease({
       owner: payload.repository.owner.login,
@@ -41,3 +52,4 @@ function makeRelease(on) {
 }
 
 module.exports = makeRelease;
+/* eslint-enable */
